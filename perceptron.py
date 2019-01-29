1b26cc5427e1e0cdc8ebc7e6263ec910fa2c9d3a
moosa <moosa.hassan@gmail.com>
2019-01-29 08:58:50 -0800

    perceptron

--- perceptron.py
+++ perceptron.py
@@ -19,8 +19,17 @@ def prediction(X, W, b):
 # the weights W (as an array), and the bias b,
 # update the weights and bias W, b, according to the perceptron algorithm,
 # and return W and b.
-def perceptronStep(X, y, W, b, learn_rate=0.01):
-    # Fill in code
+def perceptronStep(X, y, W, b, learn_rate = 0.01):
+    for i in range(len(X)):
+        y_hat = prediction(X[i],W,b)
+        if y[i]-y_hat == 1:
+            W[0] += X[i][0]*learn_rate
+            W[1] += X[i][1]*learn_rate
+            b += learn_rate
+        elif y[i]-y_hat == -1:
+            W[0] -= X[i][0]*learn_rate
+            W[1] -= X[i][1]*learn_rate
+            b -= learn_rate
     return W, b
 
 
